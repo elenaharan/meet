fc790485bce73ec1708075dc18d8544b4766db32
Elena Haran <elenaharan@gmail.com>
2021-10-14 16:35:37 +0100

    installed google apis

---
+++ auth-server/handler.js
@@ -0,0 +1,44 @@
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+
+/*SCOPES allows to set acces levels; this is set to readonly for now because you don't have access rights to update calendar yourself*/
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+/*Credentials are values required to get access to your calendar. If you see "process.end" this means the value is in the "config.json" file. This is the best practice as it keeps API secrets hidden.*/
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: ["https://elenaharan.github.io/meet"],
+  javascript_origins: ["https://elenaharan.github.io", "http://localhost:3000"],
+};
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
+/*The first step in the OAuth process is to generate a URL so users can log in with Google and be authorized to see your calendar. After logging in, they'll receive a code as a URL parameter*/
+module.exports.getAuthURL = async () => {
+  /*Scopes array passed to the `scope` option. Any scopes passed must be enabled in the "OAuth consent screen" settings in your project on your Google console. Also, any passed scopes are the ones users will see when the consent screen is displayed to them*/
+  const authURL = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authURL: authURL,
+    }),
+  };
+};
