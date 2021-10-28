b331e386cc6112596998b1da50d9de21030b8b94
Elena Haran <elenaharan@gmail.com>
2021-10-28 11:22:33 +0100

    added functionality for update number of events feature

--- src/App.js
+++ src/App.js
@@ -6,52 +6,68 @@ import NumberOfEvents from "./NumberOfEvents";
 import { getEvents, extractLocations } from "./api";
 import './nprogress.css';
 
+
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numOfEvents: 32
+    numOfEvents: 32,
+    currentLocation: "all"
   }
   
 
-  updateEvents = (location) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-        this.setState({
-        events: locationEvents
-      });
-    });
-  }
-
-  updateEventCount = (eventCount) => {
-    const locations = this.state;
-    this.setState({
-      numOfEvents: eventCount
-    });
-    this.updateEvents(locations, eventCount);
-  }
+  
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events: events.slice(0, this.state.numberOfEvents), locations: extractLocations(events)});
-      }
-      
+        this.setState({ 
+          events: events.slice(0, this.state.numOfEvents), 
+          locations: extractLocations(events)
+        });
+      }      
     });
   }
+
+
+  updateEvents = (location, eventCount) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      const eventsToShow = locationEvents.slice(0, this.state.numOfEvents);
+      if (this.mounted) {
+        this.setState({
+          events: eventsToShow,
+          currentLocation: location,
+        });
+      }
+        
+    });
+  }
+
+
+  updateEventCount = async (event) => {
+    const newVal = event.target.value ? event.target.value : '32';
+    await this.setState({
+      numOfEvents: newVal
+    });
+    this.updateEvents(this.state.currentLocation, this.state.numOfEvents);
+  };
+
+  
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
   render() {
+   
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents numOfEvents={this.state.numOfEvents} />
+        <NumberOfEvents numOfEvents={this.state.numOfEvents} updateEventCount={this.updateEventCount} />
         <EventList events={this.state.events} />        
       </div>
     );
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
-    state = {
-        numOfEvents: 32,
-    };
-    
-    handleInputChange = (event) => {
-        this.setState({
-            numOfEvents: event,
-        });
-    };
+  state = {
+      numOfEvents: 32
+  }
 
-
-   render() {
-       const numOfEvents = this.state.numOfEvents;
-       return (
-       <div className="NumberOfEvents">
-           <input type="number" className="events-number" value={numOfEvents} onChange={(event) => this.handleInputChange(event.target.value)}/>
-       </div>
-       );
-   } 
+  handleNumberChanged = (event) => {
+      const number = event.target.value;
+      this.setState({
+          numOfEvents: number
+      });
+      this.props.updateEventCount(event.target.value);
+      }
+  
+  render() {
+    return (
+        <div className="NumberOfEvents">
+            <input type="number" className="events-number" value={this.state.numOfEvents} onChange={(event) => this.handleNumberChanged(event)}
+            />
+        </div>
+        );
+  }
 }
+   
 
 export default NumberOfEvents;
