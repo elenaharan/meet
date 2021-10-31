7de38d29535e5547c3cb2c30df02836b9371fb5e
Elena Haran <elenaharan@gmail.com>
2021-10-31 09:34:57 +0000

    Implemented Feature2 scenario2 E2E test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 //Feature 2 "Show/Hide event details"
 
 describe('show/hide an event details', () => {
-    //Scenario 1
+    //Scenario 1 Elements collapsed by defult
     beforeAll(async () => {
         jest.setTimeout(300000);
     });
@@ -19,5 +19,17 @@ describe('show/hide an event details', () => {
       const eventDetails = await page.$('.event .event-description');
       expect(eventDetails).toBeNull();
       browser.close();
+  });
+
+  //Scenario 2 User clicks on Details button and element expands
+  test('User can expand an event to see its details', async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.event');
+      await page.click('.showMore');
+      const eventDetails = await page.$('.event .event-description');
+      expect(eventDetails).toBeDefined();
+      browser.close();
   })
-})
+});
