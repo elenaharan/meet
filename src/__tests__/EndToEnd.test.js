9101d3fe3f4e7220b18ad3a29ee944f9a06bf8a7
Elena Haran <elenaharan@gmail.com>
2021-10-31 10:58:47 +0000

    Feature 2 scenario 3 E2E testing implemented

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,33 +3,37 @@ import puppeteer from "puppeteer";
 //Feature 2 "Show/Hide event details"
 
 describe('show/hide an event details', () => {
-    //Scenario 1 Elements collapsed by defult
+    let browser;
+    let page;
     beforeAll(async () => {
-        jest.setTimeout(300000);
+      jest.setTimeout(300000);
+      browser = await puppeteer.launch();
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.event');
     });
 
+    afterAll(() => {
+        browser.close();
+    });
+
+  //Scenario 1 Elements collapsed by defult
   test('An event element is collapsed by default', async () => { 
-    const browser = await puppeteer.launch();
-
-      const page = await browser.newPage();
-      await page.goto('http://localhost:3000/');
-
-      await page.waitForSelector('.event');
-
-      const eventDetails = await page.$('.event .event-description');
-      expect(eventDetails).toBeNull();
-      browser.close();
+    const eventDetails = await page.$('.event .event-description');
+    expect(eventDetails).toBeNull();
   });
 
   //Scenario 2 User clicks on Details button and element expands
   test('User can expand an event to see its details', async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto('http://localhost:3000/');
-      await page.waitForSelector('.event');
-      await page.click('.showMore');
+    await page.click('.showMore');
+    const eventDetails = await page.$('.event .event-description');
+    expect(eventDetails).toBeDefined();  
+  });
+
+  //Scenario 3 User can collapse an event to hide details
+  test('User can collapse an event to hide its details', async () => {
+      await page.click('.showLess');
       const eventDetails = await page.$('.event .event-description');
-      expect(eventDetails).toBeDefined();
-      browser.close();
+      expect(eventDetails).toBeNull();
   })
 });
