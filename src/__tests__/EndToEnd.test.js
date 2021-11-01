d0345d94a0cfde19de92250ef8f4d11b1cbcf210
Elena Haran <elenaharan@gmail.com>
2021-11-01 21:55:37 +0000

    Integrated Atatus

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -6,7 +6,7 @@ describe('show/hide an event details', () => {
     let browser;
     let page;
     beforeAll(async () => {
-      jest.setTimeout(300000);
+      
       browser = await puppeteer.launch();
       page = await browser.newPage();
       await page.goto('http://localhost:3000/');
@@ -19,20 +19,23 @@ describe('show/hide an event details', () => {
 
   //Scenario 1 Elements collapsed by defult
   test('An event element is collapsed by default', async () => { 
+    jest.setTimeout(300000);
     const eventDetails = await page.$('.event .event-description');
     expect(eventDetails).toBeNull();
   });
 
   //Scenario 2 User clicks on Details button and element expands
   test('User can expand an event to see its details', async () => {
-    await page.click('.showMore');
+    jest.setTimeout(300000);
+    await page.click('.event .showMore');
     const eventDetails = await page.$('.event .event-description');
     expect(eventDetails).toBeDefined();  
   });
 
   //Scenario 3 User can collapse an event to hide details
   test('User can collapse an event to hide its details', async () => {
-      await page.click('.showLess');
+    jest.setTimeout(300000);  
+    await page.click('.event .showLess');
       const eventDetails = await page.$('.event .event-description');
       expect(eventDetails).toBeNull();
   });
