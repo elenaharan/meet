94e75c893d05d0a202913ff46c33d7f77a541c34
Elena Haran <elenaharan@gmail.com>
2021-11-06 13:46:56 +0000

    Implemented ErrorAlert in NumberOfEvents component

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-      numOfEvents: 32
+      numOfEvents: 32,
+      infoText: ''
   }
 
   handleNumberChanged = (event) => {
-      const value = event.target.value;
-      this.setState({
-          numOfEvents: value
-      });
-      this.props.updateEventCount(event.target.value);
-      }
+      const number = event.target.value;
+      if (number <= 0 || number > 32) {
+        return this.setState({
+          numOfEvents: number,
+          infoText: "Please enter a number between 1 and 32"
+        });
+      } else {
+            this.setState({
+                    numOfEvents: number,
+                    infoText: ''
+                });
+            this.props.updateEventCount(event.target.number);
+            }
+        };    
+    
   
   render() {
     return (
         <div>
+            <ErrorAlert text={this.state.infoText} />
             <p>Limit your search results</p>
             <input type="number" className="events-number" value={this.state.numOfEvents} onChange={(e) => this.handleNumberChanged(e)}
             />
