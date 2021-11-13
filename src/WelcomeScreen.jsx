64e707bcf8aa72eaae5851f455b3eb6ced0d6deb
Elena Haran <elenaharan@gmail.com>
2021-11-13 16:48:15 +0000

    minor styling changes

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -6,12 +6,12 @@ import { Container } from "react-bootstrap";
 function WelcomeScreen(props) {
 return props.showWelcomeScreen ?
 (
-<Container className="WelcomeScreen">
-<img alt="background" src={background} className="background" />
-<div className="WelcomeScreen">
-<h1>Welcome to the Meet app</h1>
-<h4>
-Log in to see upcoming events around the world for
+  <Container className="WelcomeScreen">
+  <img alt="background" src={background} className="background" />
+  <div className="WelcomeScreen">
+    <h1>Welcome to the Meet app</h1>
+    <h4>
+    Log in to see upcoming events around the world for
 full-stack
 developers
 </h4>
