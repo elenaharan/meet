582620ebe57596a66bc74a64994c5c153bd70ccf
Elena Haran <elenaharan@gmail.com>
2021-11-14 17:05:23 +0000

    minor adjustments

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -12,14 +12,15 @@ return props.showWelcomeScreen ?
   <div className="WelcomeScreen">
     <h1>Welcome to the Meet app</h1>
     <h4>
-    Log in to see upcoming events around the world for full-stack developers
+    Log in to see upcoming events <br>
+    around the world for full-stack developers </br>
     </h4>
     <div className="button_cont" align="center">
       <div class="google-btn">
         <div class="google-icon-wrapper">
           <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
         </div>
-        <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener" class="btn-text">
+        <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener" className="btn-text">
           <b>Sign in with google</b>
         </button>
         </div>
