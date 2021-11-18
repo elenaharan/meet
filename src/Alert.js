5039280cbff6736035982c9aec93994d9fa89a90
Elena Haran <elenaharan@gmail.com>
2021-11-18 18:08:16 +0000

    added visibility parameter to alerts

--- src/Alert.js
+++ src/Alert.js
@@ -14,10 +14,10 @@ class Alert extends Component {
 
     render() {
         return (
-          <div className="Alert">
+          <div className={`Alert ${this.props.text ? "visible" : "invisible"} ${this.type}`}>
               <p style={this.getStyle()}>{this.props.text}</p>
           </div> 
-        )
+        );
     }
 }
 
